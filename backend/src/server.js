a72db4c0349cfe65f9d36845d7fc1d56b0ce6518
Sachin <gusainfitness@gmail.com>
2025-10-05 22:48:36 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,29 @@
-import express from 'express';
-const app = express();
-
+import express from "express";
+import path from "path";
 import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 dotenv.config();
-const PORT = process.env.PORT || 3000 ;
 
-import authRoutes from "./routes/auth.route.js"; 
-import messageRoutes from "./routes/message.route.js"; 
+const app = express();
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 3000;
 
+// Middlewares / Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-
-
-app.listen(PORT, () => console.log("sever is running on port 3000: " + PORT));
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
