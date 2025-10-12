606c9abaebbab5017f5e224454175197ec238330
Sachin <gusainfitness@gmail.com>
2025-10-12 16:51:45 +0530

    Welcome email implemented

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
+import {ENV} from "./env.js";
 
 export const connectDB = async () => {
     try {
-        const { MONGO_URI } = process.env;
+        const { MONGO_URI } = ENV;
         if(!MONGO_URI) throw new Error("MONGO_URI not safe");
 
         const conn = await mongoose.connect(process.env.MONGO_URI)
