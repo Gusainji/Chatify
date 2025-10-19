71863c88d2f6c3d32e29f54174affcfe60f179c2
Sachin <gusainfitness@gmail.com>
2025-10-19 11:37:58 +0530

    message endpoint is complete

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,17 +1,15 @@
-import mongoose from 'mongoose';
-import {ENV} from "./env.js";
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
-    try {
-        const { MONGO_URI } = ENV;
-        if(!MONGO_URI) throw new Error("MONGO_URI not safe");
+  try {
+    const { MONGO_URI } = ENV;
+    if (!MONGO_URI) throw new Error("MONGO_URI is not set");
 
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log('MongoDB connected',conn.connection.host);
-        
-    } catch (error) {
-        console.log("Error connection mongoose",error);
-        process.exit(1);  //1status code for failure and 0 for success
-        
-    }
-}
+    const conn = await mongoose.connect(ENV.MONGO_URI);
+    console.log("MONGODB CONNECTED:", conn.connection.host);
+  } catch (error) {
+    console.error("Error connection to MONGODB:", error);
+    process.exit(1); // 1 status code means fail, 0 means success
+  }
+};
